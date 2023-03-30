9d4ed75127cc3a35463c7920a2ea6c19a74f7309
Claribel Diaz <128192485+Clari002@users.noreply.github.com>
2023-03-30 15:02:12 -0300

    Se agrego puntero

--- tp1_1.c
+++ tp1_1.c
@@ -2,6 +2,13 @@
 
 int main()
 {
+    int *punt, num=2;
+    punt = &num;
     printf("Hola mundo");
+    printf("\n:El contenido del puntero: %d ", *punt);
+    printf("\n:La direccion de memoria almacenada por el puntero: %p ", punt);
+    printf("\n:La dirección de memoria de la variable: %p ", &num);
+    printf("\n:La dirección de memoria del puntero: %p", &punt);
+    printf("\n:El tamaño de memoria utilizado por esa variable: %d", sizeof(num));
     return 0;
 }
